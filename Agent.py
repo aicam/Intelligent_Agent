d580aad2d205dc27810cb860cce716f0d4c94faa
ali <a_icam@yahoo.com>
2021-01-02 10:55:02 +0330

    agent

--- Agent.py
+++ Agent.py
@@ -11,5 +11,13 @@ class Agent:
 
     def generate_guess(self):
         s = np.random.normal(0, self.Sigma, 2000)
-        _, bins = np.histogram(s, 300, density=True)
-        return bins, 1/(self.Sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins)**2 / (2 * self.Sigma**2))
+        count, bins = np.histogram(s, 1000, density=True)
+        return bins, count
+
+    def goal_test(self, ans, bins, count):
+        ideal = 1/(ans * np.sqrt(2 * np.pi)) * np.exp( - (bins)**2 / (2 * ans**2))
+        return np.var(count - ideal)
+
+    def record_history(self, fitness, iteration):
+        self.History.append({'Fitness': fitness, 'Iteration': iteration, 'sigma': self.Sigma})
+
