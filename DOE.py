79fad51073e9a45bf44d1e77d649624cbb33d707
ali <a_icam@yahoo.com>
2021-01-13 13:26:45 +0330

    DOE

--- DOE.py
+++ DOE.py
@@ -13,12 +13,12 @@ def generate_random_attribute(p, shape):
     attributes = np.zeros(shape)
     for i in range(attributes.shape[0]):
         attributes[i] = np.array([0 if t > p[i] else 1 for t in np.random.rand(attributes.shape[1])])
-    return attributes
+    return attributes.astype(np.int)
 
 def generate_estimation(attr):
-    time_estimation = len(attr) + 1
+    time_estimation = (len(attr) + 1)/2
     for i in range(len(attr)):
-        time_estimation += np.random.uniform(-1, 0) if attr[i] == (i%3 == 0) else np.random.uniform(0, 1)
+        time_estimation += np.random.uniform(-.5, 0) if attr[i] == (i%3 == 0) else np.random.uniform(0, .5)
     time_estimation += np.random.uniform(-1, 1)
     return int(time_estimation*1000)
 
@@ -29,5 +29,8 @@ for i in range(total_count):
     es_time = generate_estimation(attributes[:,i])
     df.loc[i] = [i,
                  es_time,
-                 1 if np.random.uniform(0, 1) < (1 - np.e**es_time) else 0,
+                 1 if np.random.uniform(0, .8) < (1 - np.e**(- int(es_time/1000))) else 0,
+                 ''.join(attributes[:,i].astype(np.str))
                  ]
+
+df.to_csv('DOE.csv')
